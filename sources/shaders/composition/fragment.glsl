ff9cd228f70120bf2fd2ee401ae6177868cc259b
Kiril Tucker <kiril.tucker@gmail.com>
2022-10-12 18:13:01 +0200

    Post-processing: Vignette Effect

--- sources/shaders/composition/fragment.glsl
+++ sources/shaders/composition/fragment.glsl
@@ -4,6 +4,9 @@ uniform sampler2D uDefaultTexture;
 uniform sampler2D uDistortionTexture;
 uniform vec2 uConvergencePosition;
 
+#include ../partials/inverseLerp.glsl
+#include ../partials/remap.glsl
+
 void main() {
   float distortionStrength = texture(uDistortionTexture, vUv).r;
   vec2 convergencePoint = vec2(0.5);
@@ -11,5 +14,12 @@ void main() {
   vec2 distoredUv = vUv + toConvergence * distortionStrength;
 
   vec4 color = texture(uDefaultTexture, distoredUv);
+
+  // Vignette
+  float distanceToCenter = length(vUv - 0.5);
+  float vignetteStrength = remap(distanceToCenter, 0.3, 0.7, 0.0, 1.0);
+  vignetteStrength = smoothstep(0.0, 1.0, vignetteStrength);
+  color.rgb = mix(color.rgb, vec3(0.0), vignetteStrength);
+
   gl_FragColor = color;
 }
